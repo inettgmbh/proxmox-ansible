17eacbea27a5feaeb82b5f501bd1fbfd0ddc9e62
Maximilian Hill <mhill@inett.de>
2021-11-05 11:24:30 +0100

    added support for iothread and ssd options on scsi devices

--- plugins/modules/pve_vm_disks.py
+++ plugins/modules/pve_vm_disks.py
@@ -25,20 +25,22 @@ def run_module():
     mod = PveApiModule(argument_spec=arg_spec, supports_check_mode=True)
     vm, vm_config = mod.vm_config_get(mod.params['vmid'])
     update_params = dict()
+    update_args = dict()
 
     for k, s in mod.params.get('scsi', dict()).items():
-        s_key = "scsi%s" % k
-        if s_key not in vm_config:
-            update_params[s_key] = dict(
-                file="%s:%d" % (
-                    s.get('storage', mod.params.get('storage')),
-                    s.get('size', 32)
-                ),
-                cache=s.get('cache', 'writeback'),
-                discard=('on' if s.get('discard', True) else 'ignore'),
-            )
+        update_args["scsi%s" % k] = s
+        update_params["scsi%s" % k] = dict(
+            file="%s:%d" % (
+                s.get('storage', mod.params.get('storage')),
+                s.get('size', 32)
+            ),
+            cache=s.get('cache', 'writeback'),
+            discard=('on' if s.get('discard', True) else 'ignore'),
+            iothread=('on' if s.get('iothread', True) else 'ignore'),
+            ssd=('on' if s.get('ssd', True) else 'ignore'),
+        )
 
-    message = update_params;
+    message = update_params
 
     old_message = dict({k: vm_config.get(k, None) for (k, v) in update_params.items()})
     changed = (old_message != message)
@@ -50,12 +52,12 @@ def run_module():
             config=update_params,
             vm=vm
         )
+
     mod.exit_json(
         changed=changed,
         message=message,
         original_message=old_message
     )
-    return
 
 
 def main():
